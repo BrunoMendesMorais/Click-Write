8f02c1876af973deff770e792d8d994301f8d9ff
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2025-07-16 19:27:49 -0300

    animação na tela escrever

--- script/escrever.js
+++ script/escrever.js
@@ -1,28 +1,28 @@
-document.getElementById('escreverCarta').addEventListener('submit', function (event) {
-    event.preventDefault(); // impede o envio padrão
+const carta = document.getElementById("carta");
+const cortina = document.getElementById("cortina");
+const body = document.getElementById("body");
+const cortinaContent = document.getElementById("cortinaContent");
 
-    // Coletar os valores dos inputs
+document.getElementById('escreverCarta').addEventListener('submit', function (event) {
+    event.preventDefault();
+
     const remetente = document.getElementById('remetente').value;
     const data = document.getElementById('data').value;
     const destinatario = document.getElementById('destinatario').value;
     const email = document.getElementById('email').value;
     const titulo = document.getElementById('titulo').value;
     const mensagem = document.getElementById('mensagem').value;
-
-
-    // Preencher a seção "carta" com os dados
+    carta.style.display = "grid";
+    carta.classList.add("escrever")
     document.getElementById('remetenteCarta').textContent = remetente;
     document.getElementById('dataCarta').textContent = data;
     document.getElementById('destinatarioCarta').textContent = destinatario;
     document.getElementById('emailCarta').textContent = email;
     document.getElementById('tituloCarta').textContent = titulo;
     document.getElementById('mensagemCarta').textContent = mensagem;
+
 });
 
-const carta = document.getElementById("carta");
-const cortina = document.getElementById("cortina");
-const body = document.getElementById("body");
-const cortinaContent = document.getElementById("cortinaContent");
 
 function expandir() {
     cortinaContent.innerHTML = carta.innerHTML;
